add9b012bb0643e573a7ac6c7f04e2ba36ef4b1a
tom0521 <tsherwood99@gmail.com>
2019-08-04 20:04:17 -0400

    Created json-query.php

--- Webpage/php/data.php
+++ Webpage/php/data.php
@@ -2,14 +2,8 @@
 include 'connect.php';
 
 $sql = "SELECT MONTHNAME(date) AS month, MONTH(date), SUM(amount) AS amount FROM income GROUP BY MONTHNAME(date), MONTH(date) ORDER BY MONTH(date)";
-$result = $conn->query($sql);
 
-$rows = array();
-if ($result->num_rows > 0) {
-	while ($row = $result->fetch_assoc()) {
-		$rows[] = $row;
-	}
-}
-echo json_encode($rows);
+include 'json_query.php';
+
 $conn->close();
 ?>
