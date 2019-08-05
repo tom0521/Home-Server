82c54808bd4886ff875b41c835e9ee768dbddb41
tom0521 <tsherwood99@gmail.com>
2019-08-04 20:33:58 -0400

    Added a spending graph

--- Webpage/php/data.php
+++ Webpage/php/data.php
@@ -3,7 +3,7 @@ include 'connect.php';
 
 $sql = "SELECT MONTHNAME(date) AS month, MONTH(date), SUM(amount) AS amount FROM income GROUP BY MONTHNAME(date), MONTH(date) ORDER BY MONTH(date)";
 
-include 'json_query.php';
+include 'json-query.php';
 
 $conn->close();
 ?>
