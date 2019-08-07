573a758c584321a957b68353f74fe7db00e3aebb
tom0521 <tsherwood99@gmail.com>
2019-08-06 22:09:44 -0400

    HTML5 image input tag

--- Apache/Upload_site/php/upload.php
+++ Apache/Upload_site/php/upload.php
@@ -20,7 +20,7 @@ if (file_exists($target_file)) {
 	$uploadOk = 0;
 }
 
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 20000000) {
 	echo "Sorry, your file is too large.";
 	$uploadOk = 0;
 }
