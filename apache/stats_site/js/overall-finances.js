931e38c0c334664bee2b91a4a40b21da31e2b937
tom0521 <tsherwood99@gmail.com>
2019-08-17 18:00:51 -0400

    Overal stats now have commas in appropriate places

--- apache/stats_site/js/overall-finances.js
+++ apache/stats_site/js/overall-finances.js
@@ -1,5 +1,9 @@
 $.getJSON('php/overall-finances.php', function(jsonData) {
-	document.getElementById('gross-income').innerHTML = `$${jsonData.gross_income}`;
-	document.getElementById('expenses').innerHTML = `$${jsonData.expenses}`;
-	document.getElementById('net-income').innerHTML = `$${jsonData.net_income}`;
+	document.getElementById('gross-income').innerHTML = `$${numberWithCommas(jsonData.gross_income)}`;
+	document.getElementById('expenses').innerHTML = `$${numberWithCommas(jsonData.expenses)}`;
+	document.getElementById('net-income').innerHTML = `$${numberWithCommas(jsonData.net_income)}`;
 });
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
