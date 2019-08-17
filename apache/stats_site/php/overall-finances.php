2f10bf7261fa16a2558ed980f484a1455c628a4f
tom0521 <tsherwood99@gmail.com>
2019-08-16 21:15:44 -0400

    Created a php script to query the overall financial data (Should eventually be combined with the other query)

---
+++ apache/stats_site/php/overall-finances.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'connect.php';
+
+$sql = "SELECT income.gross_income, expenses.expenses, income.gross_income - expenses.expenses AS net_income
+	FROM (
+		SELECT SUM(amount) as gross_income FROM finances.income
+	) AS income,
+	(
+		SELECT SUM(price) as expenses FROM finances.spending
+	) AS expenses";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	$row = $result->fetch_assoc();
+	echo json_encode(array(
+		"gross_income"=>$row['gross_income'],
+		"expenses"=>$row['expenses'],
+		"net_income"=>$row['net_income']));
+}
+
+$conn->close();
+
+?>
