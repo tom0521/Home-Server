6ba63037af586c6a14553646bf3b4e1384bc5dd2
tom0521 <tsherwood99@gmail.com>
2019-08-16 20:36:26 -0400

    Combined the cash-flow.php query into a single SQL query

--- apache/stats_site/php/cash-flow.php
+++ apache/stats_site/php/cash-flow.php
@@ -1,7 +1,17 @@
 <?php
 include 'connect.php';
 
-$sql = "SELECT MONTHNAME(date) AS month, MONTH(date), SUM(amount) AS amount FROM income GROUP BY MONTHNAME(date), MONTH(date) ORDER BY MONTH(date)";
+$sql = "SELECT income.month AS month,
+         income.income AS income, expenses.expenses AS expenses
+         FROM (
+         	SELECT MONTH(date) AS month, SUM(amount) as income
+         	FROM finances.income GROUP BY MONTH(date) ORDER BY MONTH(date)
+         ) AS income,
+         (
+         	SELECT MONTH(timestamp) AS month, SUM(price) as expenses
+         	FROM finances.spending GROUP BY MONTH(timestamp) ORDER BY MONTH(timestamp)
+         ) AS expenses
+         WHERE income.month=expenses.month";
 
 $result = $conn->query($sql);
 
@@ -27,29 +37,16 @@ $jsonObj = array(
 );
 
 if ($result->num_rows > 0) {
+	$prev = 0; 
 	while($row = $result->fetch_assoc()) {
 		$jsonObj['labels'][] = $row['month'];
-		$jsonObj['datasets'][0]['values'][] = $row['amount'];
+		$jsonObj['datasets'][0]['values'][] = $row['income'];
+		$jsonObj['datasets'][1]['values'][] = $row['expenses'];
+		$prev += $row['income'] - $row['expenses'];
+		$jsonObj['datasets'][2]['values'][] = $prev;
 	}
 }
 
-$sql = "SELECT MONTHNAME(timestamp) AS month, MONTH(timestamp), SUM(price) AS amount FROM spending GROUP BY MONTHNAME(timestamp), MONTH(timestamp) ORDER BY MONTH(timestamp)";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		$jsonObj['datasets'][1]['values'][] = $row['amount'];
-	}
-}
-
-for($i=0; $i<$result->num_rows; $i++){
-	if($i!=0){
-		$prev = $jsonObj['datasets'][2]['values'][$i-1];
-	} else {
-		$prev = 0;
-	}
-	$jsonObj['datasets'][2]['values'][] = $prev + ($jsonObj['datasets'][0]['values'][$i] - $jsonObj['datasets'][1]['values'][$i]);
-}
 
 echo json_encode($jsonObj);
 
