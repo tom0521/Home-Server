023e8880d094f22da0d82ed09e842dae06d15182
tom0521 <tsherwood99@gmail.com>
2019-08-21 22:20:48 -0400

    Moved expenses.js away from JQuery

--- apache/stats_site/js/expenses.js
+++ apache/stats_site/js/expenses.js
@@ -1,8 +1,28 @@
-$.getJSON('php/expenses.php', function(jsonData){
+/*$.getJSON('php/expenses.php', function(jsonData){
 	const chart = new frappe.Chart("#expenses-pie", {
 		title: "Expenses",
 		data: jsonData,
 		type: 'pie',
 		height: (screen.height / 2.5)
 	});
-});
+});*/
+
+var request = new XMLHttpRequest();
+request.open('GET', 'php/expenses.php', true);
+request.onload = function () {
+	if (this.status >= 200 && this.status < 400) {
+		var jsonData = JSON.parse(this.response);	
+		const chart = new frappe.Chart('#expenses-pie', {
+			title: 'Expenses',
+			data: jsonData,
+			type: 'pie',
+			height: (screen.height / 2.5)
+		});
+	} else {
+		console.log('Error getting expense data');
+	}
+};
+request.onerror = function() {
+	console.log('Error getting expense data');
+};
+request.send();
