0f222afbae563f43e73f18efbcc40edd633920da
tom0521 <tsherwood99@gmail.com>
2020-12-11 17:34:18 -0500

    changed 501 responses to abort(501)

--- python/resource.py
+++ python/resource.py
@@ -3,59 +3,59 @@ from flask_restful import Resource
 
 class Account(Resource):
     def get(self, id=None):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
     
     def post(self, id=None):
         if id:
             abort(404)
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
     def udpate(self, id=None):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
 class Address(Resource):
     def get(self, id=None):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
     
     def post(self, id=None):
         if id:
             abort(404)
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
     def udpate(self, id=None):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
 class Category(Resource):
     def get(self, id=None):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
     
     def post(self, id=None):
         if id:
             abort(404)
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
     def udpate(self, id=None):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
 class City(Resource):
     def get(self):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
     
     def post(self):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
     def udpate(self):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
 class Place(Resource):
     def get(self):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
     
     def post(self):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
     def udpate(self):
-        return { 'message': 'Not Implemented' }, 501
+        abort(501)
 
 class Transaction(Resource):
     def get(self, id=None):
