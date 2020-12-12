c5b51bfb64c7ad89a15d5f2014191ff583261804
tom0521 <tsherwood99@gmail.com>
2020-12-12 12:06:07 -0500

    /address done

--- python/main.py
+++ python/main.py
@@ -46,6 +46,9 @@ class Address(db.Model):
     url = db.Column(db.String(255))
     transactions = db.relationship('Transaction', backref='address', lazy=True)
 
+    def __repr__(self):
+        return f'{self.address} {self.address2}\n{self.url}'
+
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(50), unique=True, nullable=False)
@@ -140,13 +143,62 @@ class AccountApi(Resource):
         return marshal(account, self.mfields), 201
 
 class AddressApi(Resource):
+    mfields = {
+        'id': fields.Integer,
+        'place_id': fields.Integer,
+        'address': fields.String,
+        'address2': fields.String,
+        'city_id': fields.Integer,
+        'postal_code': fields.String,
+        'phone': fields.String,
+        'url': fields.String
+    }
+
     def get(self, id=None):
-        abort(501)
+        # if the id was specified, try to query it
+        if id:
+            address = Address.query.filter_by(id=id).first()
+            if address:
+                return marshal(address, self.mfields), 200
+            abort(404)
+        return marshal(Address.query.all(), self.mfields), 200
     
     def post(self, id=None):
+        # POST requests do not allow id url
         if id:
             abort(404)
-        abort(501)
+
+        # set the arguments for the request
+        parser = reqparse.RequestParser()
+        parser.add_argument('place_id', type=int, required=True)
+        parser.add_argument('address')
+        parser.add_argument('address2')
+        parser.add_argument('city_id', type=int)
+        parser.add_argument('postal_code')
+        parser.add_argument('phone')
+        parser.add_argument('url')
+        args = parser.parse_args()
+
+        # if any foreign ids do not exist abort
+        if Place.query.filter_by(id=args['place_id']).first() is None:
+            abort(400)
+        if args['city_id'] and City.query.filter_by(id=args['city_id']).first is None:
+            abort(400)
+
+        # If the etnry already exists, return the entry with Accepted status code
+        address = Address.query.filter_by(place_id=args['place_id'], address=args['address'],
+                    address2=args['address2'], city_id=args['city_id'], postal_code=args['postal_code'],
+                    phone=args['phone'], url=args['url']).first()
+        if address:
+            return marshal(address, self.mfields), 202
+
+        # Otherwise, insert the new entry and return Created status code
+        address = Address(place_id=args['place_id'], address=args['address'],
+                    address2=args['address2'], city_id=args['city_id'], 
+                    postal_code=args['postal_code'], phone=args['phone'], url=args['url'])
+        db.session.add(address)
+        db.session.commit()
+        return marshal(address, self.mfields), 201
 
 class CategoryApi(Resource):
     def get(self, id=None):
