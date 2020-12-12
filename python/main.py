ff6dd201af216b73a8dc9b7bc428cfacf3055e78
tom0521 <tsherwood99@gmail.com>
2020-12-11 20:29:49 -0500

    Started sql connection; added resource .qualifer

--- python/main.py
+++ python/main.py
@@ -5,14 +5,24 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
-from resource import Account,Address,Category
+import resource
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-api.add_resource(Account, '/account', '/account/<int:id>')
-api.add_resource(Address, '/address', '/address/<int:id>')
-api.add_resource(Category, '/category', '/category/<int:id>')
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account = db.Column(db.String(50), unique=True, nullable=False)
+    balance = db.Column(db.Float, server_default='0')
+    
+    def __repr__(self):
+        return '%r - $%.2f' % (self.account, self.balance)
+
+api.add_resource(resource.Account, '/account', '/account/<int:id>')
+api.add_resource(resource.Address, '/address', '/address/<int:id>')
+api.add_resource(resource.Category, '/category', '/category/<int:id>')
 
 @app.route('/')
 def index():
