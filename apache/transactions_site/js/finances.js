83075b8db76710734c88c8162fbe2ffdbe448171
tom0521 <tsherwood99@gmail.com>
2020-12-18 16:10:35 -0500

    Website now using bootstrap

--- apache/transactions_site/js/finances.js
+++ apache/transactions_site/js/finances.js
@@ -1,5 +1,4 @@
 function init () {
-
 }
 
 function create () {
@@ -23,9 +22,19 @@ function create () {
     data.place_id = create_place(data).id;
     data.city_id = create_city(data).id;
     data.address_id = create_address(data).id;
-    data.account_id = create_account(data).id;
+    // data.account_id = create_account(data).id;
     data.category_id = create_category(data).id;
     create_transaction(data);
+}
+
+function show_transaction_popup () {
+    $('#transaction-popup').css('display', 'block');
+    $('#transaction-popup-btn').css('display', 'none');
+}
+
+function hide_transaction_popup () {
+    $('#transaction-popup').css('display', 'none');
+    $('#transaction-popup-btn').css('display', 'block');
 }
 
 document.addEventListener('DOMContentLoaded', init);
--- apache/transactions_site/js/popup.js
+++
@@ -1,7 +0,0 @@
-function open_form () {
-    $('#transaction-popup').css('display', 'block');
-}
-
-function close_form () {
-    $('#transaction-popup').css('display', 'none');
-}
