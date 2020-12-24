d95db8e70d3f579017f86be149b1e0964d689b4c
tom0521 <tsherwood99@gmail.com>
2020-12-23 22:50:12 -0500

    added a simple pie chart

--- apache/transactions_site/js/finances.js
+++ apache/transactions_site/js/finances.js
@@ -21,7 +21,61 @@ function init () {
         $('#expenses').text(currency_format.format(expenses));
         $('#net-income').text(currency_format.format(gross_income - expenses));
     });
+    init_line_graph();
     init_pie_chart();
+}
+
+function init_line_graph () {
+    var data = get_transactions().sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+    var height = 500;
+    var width = height;
+    var margin = ({top: 20, right: 30, bottom: 30, left: 40});
+    var yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.select(".tick:last-of-type text").clone()
+            .attr("x", 3)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(data.y));
+
+    var xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+    y = d3.scaleLinear()
+        .domain([d3.min(data, d => d.amount), d3.max(data, d => d.amount)]).nice()
+        .range([height - margin.bottom, margin.top]);
+
+    x = d3.scaleUtc()
+        .domain(d3.extent(data, d => new Date(d.timestamp)))
+        .range([margin.left, width - margin.right]);
+
+    line = d3.line()
+        .defined(d => !isNaN(d.amount))
+        .x(d => x(new Date(d.timestamp)))
+        .y(d => y(d.amount));
+
+    const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height]);
+
+    svg.append("g")
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("d", line);
+
+    $('#rolling-income').append(svg.node());
 }
 
 function init_pie_chart () {
