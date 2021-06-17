70b7fe9a9cb0ff297b6bee577d8ae23c7d9309e7
Thomas Sherwood <tsherwood99@gmail.com>
2021-06-17 02:49:15 +0000

    Initial DataProvider

--- node/dashboard/src/App.js
+++ node/dashboard/src/App.js
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import dataProvider from './DataProvider';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={ListGuesser} />
+        <Resource name="transaction" list={ListGuesser} />
     </Admin>
 );
 
