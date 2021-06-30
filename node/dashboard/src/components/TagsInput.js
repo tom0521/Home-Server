cae606833640dd8404cae4ffaeae8b5bdf0b676d
Thomas Sherwood <tsherwood99@gmail.com>
2021-06-30 23:05:48 +0000

    #14 Changed add chip keys

--- node/dashboard/src/components/TagsInput.js
+++ node/dashboard/src/components/TagsInput.js
@@ -2,18 +2,25 @@ import React from 'react';
 import ChipInput from 'material-ui-chip-input';
 import { useInput } from 'react-admin';
 
+const keys = [
+    'Tab',
+    'Comma'
+];
+
 const TagsInput = props => {
     const {
         input: { value, onChange, ...rest},
         meta: { touched, error }
     } = useInput(props);
-    
+   
     return (
         <ChipInput
-            source="tags"
             value={ value || [] }
             onAdd={ (tag) => onChange((value || []).concat(tag)) }
             onDelete={ (tag) => onChange((value || []).filter(v => v !== tag)) }
+            newChipKeys={ keys }
+            error={ !!(touched && error) }
+            helperText={ touched && error }
             {...rest}
         />
     );
